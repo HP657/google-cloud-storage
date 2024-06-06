611791dc00072d939f267285ebf5ce803d9d7c82
HP657 <junlee5070@gmail.com>
2024-06-06 19:49:27 +0900

    project init

---
+++ src/test/java/com/cloud/img/CloudImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.img;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudImgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
